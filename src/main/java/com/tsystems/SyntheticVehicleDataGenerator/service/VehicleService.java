bd0f92c4e40d319531ae8a11746da2767d12603c
vinayakasvinu <107644694+vinayakasvinu@users.noreply.github.com>
2023-10-31 11:25:02 +0530

    Delete src directory

--- src/main/java/com/tsystems/SyntheticVehicleDataGenerator/service/VehicleService.java
+++
@@ -1,9 +0,0 @@
-package com.tsystems.SyntheticVehicleDataGenerator.service;
-
-import com.tsystems.SyntheticVehicleDataGenerator.model.VehicleData;
-
-import java.util.List;
-
-public interface VehicleService {
-    List<VehicleData> getGeneratedData(int limit);
-}
